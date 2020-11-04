6046208b184bb6b5d23e3bdbd35032c8ffd67786
ardourApeX <joygupta501@gmail.com>
2020-11-04 21:35:39 +0530

    Aadhar Number Extraction Done

--- Aadhar.py
+++ Aadhar.py
@@ -16,6 +16,7 @@ def File_Names(path = "./images"):
 	for file in dirs:
 		labels.append(str(file)) #Store path of each image
 
+
 	return labels
 
 
@@ -29,3 +30,38 @@ def CreateFolder():
 	except:
 		# If folder is there, pass then
 		pass
+
+
+def OCR(images):
+
+	# Create a pipeline first of all
+	pipeline = keras_ocr.pipeline.Pipeline()
+
+	for image in images:
+		prediction = pipeline.recognize([image])
+
+		values = [x[0] for x in prediction[0]]
+		string = " ".join(values)
+		aadhar = re.findall(r'[2-9]{1}[0-9]{3}\s[0-9]{4}\s[0-9]{4}', string) # all the aadhar numbers found
+		try :
+
+			aadhar = aadhar[0]
+		except :
+			aadhar = "Not Found"
+
+		print(aadhar)
+
+def ReadAllImages():
+	'''This function is to load all the images'''
+	images = []
+	labels = File_Names()
+	for label in labels:
+		images.append(cv.imread(label))
+
+	# Converting all images into Grayscale
+	images = list(map(lambda x : cv.cvtColor(x, cv.COLOR_BGR2RGB), images))
+
+
+	OCR(images)
+
+ReadAllImages()
