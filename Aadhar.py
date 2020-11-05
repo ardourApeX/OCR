66cc36ee8e695708f7ce0794d1062ea6a4b9f4ae
ardourApeX <joygupta501@gmail.com>
2020-11-05 23:08:00 +0530

    Added ApplyPatch Function

--- Aadhar.py
+++ Aadhar.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import re
 import cv2 as cv
 import keras_ocr
-
+from pathlib import Path
 
 
 def File_Names(path = "./images"):
@@ -31,12 +31,39 @@ def CreateFolder():
 		# If folder is there, pass then
 		pass
 
+def ApplyPatch(aadhar, prediction):
+	# To store coordinates where we need to draw the white patches
+	rectangle = {
+	    1:[],
+	    2:[]
+	}
+
+	#CreateFolder() # Creating Folder to Store Images
+
+
+	# Checking over consequtive numbers 
+	for i in range(len(prediction[0])):
+	    if prediction[0][i][0] == aadhar[0:4]:
+	        if prediction[0][i+1][0] == aadhar[5:9]:
+	            if prediction[0][i+2][0] == aadhar[10:]:
+	                rectangle[1].append((prediction[0][i][1][0::2]))
+	                rectangle[2].append((prediction[0][i+1][1][0::2]))
+	  
+
+	# Drawing rectangle
+	for i in rectangle.keys():
+	    for x in rectangle[i]:
+	        color_img = cv.rectangle(color_img, tuple(x[0]), tuple(x[1]), color = (255, 255, 255), thickness = -1)
+
+	cv.imshow("image", color_img)
+	cv.waitKey(1000)
 
 def OCR(images):
 
 	# Create a pipeline first of all
 	pipeline = keras_ocr.pipeline.Pipeline()
 
+	file = open("Aadhar Number.txt", "a")
 	for image in images:
 		prediction = pipeline.recognize([image])
 
@@ -46,10 +73,16 @@ def OCR(images):
 		try :
 
 			aadhar = aadhar[0]
-		except :
-			aadhar = "Not Found"
+			file.write(str(aadhar) + "\n")
 
-		print(aadhar)
+
+			ApplyPatch(aadhar, prediction)
+
+		except :
+
+			pass
+
+		
 
 def ReadAllImages():
 	'''This function is to load all the images'''
@@ -63,5 +96,5 @@ def ReadAllImages():
 
 
 	OCR(images)
-
 ReadAllImages()
+
